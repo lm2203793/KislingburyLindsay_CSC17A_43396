dea0225a3bacd43d5890e7862d6703521ae614ea
lm2203793 <lindsaymoses@gmail.com>
2021-03-05 19:39:03 -0800

    Signed-off-by: lm2203793 <lindsaymoses@gmail.com>

--- Homework/Review Homework 1/Gaddis_9thEd_Chap7_Rain_Or_Shine/main.cpp
+++ Homework/Review Homework 1/Gaddis_9thEd_Chap7_Rain_Or_Shine/main.cpp
@@ -7,6 +7,7 @@
 
 //System Libraries
 #include <iostream>  //I/O Library
+#include <fstream>   //Stream Library
 using namespace std;
 
 //User Libraries
@@ -21,13 +22,88 @@ int main(int argc, char** argv) {
     //Initialize the Random Number Seed
     
     //Declare Variables
+    const int MNTH=3;
+    const int DAY=30;
+    char wthTble[MNTH][DAY];
+    char x;
+    int rainy, cloudy, sunny;
+    ifstream wData;
+    int maxmnth;
+    int curmaxC, curmaxS, curmaxR;
     
     //Initialize Variables
+    rainy=0;
+    sunny=0;
+    cloudy=0;
+    maxmnth=3;
+    curmaxC=0;
+    curmaxR=0;
+    curmaxS=0;    
     
-    //Map Inputs to Outputs -> Process
+    //Fill Array
+    cout<<"Weather Data Table."<<endl;
+    wData.open("wData.txt");
+    
+    for (int mnth=0; mnth<3; mnth++){
+        for(int day=0; day<30; day++){
+            wData>>wthTble[mnth][day];
+        }
+    }
+    
+    for (int mnth=0; mnth<3; mnth++){
+        cout<<endl;
+        for(int day=0; day<30; day++){
+            cout<<wthTble[mnth][day];
+        }
+    }
+    
+    //
+    for (int mnth=0; mnth<3; mnth++){
+        int temprainy, tempcloudy, tempsunny;
+        temprainy=0;
+        tempcloudy=0;
+        tempsunny=0;
+        for(int day=0; day<30; day++){
+            if (wthTble[mnth][day]=='R'){
+                temprainy++;
+            }
+            if(wthTble[mnth][day]=='C'){
+                tempcloudy++;
+            }
+            if (wthTble[mnth][day]=='S'){
+                tempsunny++;
+            }
+        }
+        rainy+=temprainy;
+        cloudy+=tempcloudy;
+        sunny+=tempsunny;
+        
+        if(temprainy>curmaxR){
+            maxmnth=mnth;
+            curmaxR=temprainy;
+        }
+        if(tempcloudy>curmaxC){
+            maxmnth=mnth;
+            curmaxR=tempcloudy;
+        }        
+        if(temprainy>curmaxS){
+            maxmnth=mnth;
+            curmaxR=tempsunny;
+        }
+    }
+    
+    cout<<"\ntotal rainy days = "<<rainy<<endl;
+    cout<<"total sunny days = "<<sunny<<endl;
+    cout<<"total cloudy days = "<<cloudy<<endl;
+    cout<<"Rainiest Month = "<<curmax[]
+
+
+    
+    
     
     //Display Inputs/Outputs
     
     //Exit the Program - Cleanup
     return 0;
 }
+
